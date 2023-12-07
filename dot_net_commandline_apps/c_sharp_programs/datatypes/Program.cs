937d86c423e867ed41dc04f912895385645a7811
Aditya Kumar <aryan68125@gmail.com>
2023-12-07 20:52:35 +0530

    string interpolation datatypes

--- dot_net_commandline_apps/c_sharp_programs/datatypes/Program.cs
+++ dot_net_commandline_apps/c_sharp_programs/datatypes/Program.cs
@@ -1,7 +1,11 @@
 ﻿namespace Aditya{
     class Variables{
         public static void Main(string[] args){
-            Console.WriteLine("he");
+            string phrase = "Rollex is a hunk";
+            char grade = 'A';
+            int age = 20;
+            double money = 1425.2569;
+            Console.WriteLine($"phrase : {phrase} \n grade : {grade} \n age : {age} \n money : {money}");
         }
     }
 }
