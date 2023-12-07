e16aeeb204f5154ebc4446306491994ad2450cb0
Aditya Kumar <aryan68125@gmail.com>
2023-12-07 22:00:37 +0530

    datatypes complete

--- dot_net_commandline_apps/c_sharp_programs/datatypes/Program.cs
+++ dot_net_commandline_apps/c_sharp_programs/datatypes/Program.cs
@@ -4,8 +4,12 @@
             string phrase = "Rollex is a hunk";
             char grade = 'A';
             int age = 20;
-            double money = 1425.2569;
-            Console.WriteLine($"phrase : {phrase} \n grade : {grade} \n age : {age} \n money : {money}");
+            decimal money = 1425.2569m;
+            float number_float = 1245.235f;
+            double number_double = 12.56d;
+            bool flag_true_boolean = true;
+            bool flag_false_boolean = false;
+            Console.WriteLine($"phrase : {phrase} \n grade : {grade} \n age : {age} \n money : {money} \n number_float : {number_float} \n number_double : {number_double} \n flag_true_boolean : {flag_true_boolean} \n flag_false_boolean : {flag_false_boolean}");
         }
     }
 }
