955852fa659f006cbc837475b6bf6d86c0ab0b35
Aditya Kumar <aryan68125@gmail.com>
2023-12-07 18:21:16 +0530

    variables

---
+++ dot_net_commandline_apps/c_sharp_programs/variables/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Aditya{
+    class Variables{
+        public static void Main(string[] args){
+            Console.WriteLine("There is a boy named Rollex");
+            Console.WriteLine("Rollex is a 20 year old boy");
+            Console.WriteLine("He really loves to program");
+        }
+    }
+}
