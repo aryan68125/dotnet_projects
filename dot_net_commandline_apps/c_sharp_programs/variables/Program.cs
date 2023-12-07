ff2f62559dba0f9e63ee0a6e91fd11285bbf0d91
Aditya Kumar <aryan68125@gmail.com>
2023-12-07 19:12:12 +0530

    how to print the variables with a message

--- dot_net_commandline_apps/c_sharp_programs/variables/Program.cs
+++ dot_net_commandline_apps/c_sharp_programs/variables/Program.cs
@@ -3,19 +3,19 @@
         public static void Main(string[] args){
             //using statically defined variables
             string sentence1 = "There is a boy named Rollex";
-            int number = 20;
-            double double_variable = 50000.526;
-            Console.WriteLine(sentence1);
-            Console.WriteLine(number);
-            Console.WriteLine(double_variable);
+            int number1 = 20;
+            double double_variable1 = 50000.526;
+            Console.WriteLine("string : " + sentence1);
+            Console.WriteLine("integer : " + number1);
+            Console.WriteLine("double : " + double_variable1);
 
             //using dynamically defined variables
             var sentence2 = "There is a boy named Aditya";
             var number2 = 26;
             var double_variable2 = 905621.526;
-            Console.WriteLine(sentence2);
-            Console.WriteLine(number2);
-            Console.WriteLine(double_variable2);
+            Console.WriteLine("string : " + sentence2);
+            Console.WriteLine("integer : " + number2);
+            Console.WriteLine("double : "  + double_variable2);
         }
     }
 }
