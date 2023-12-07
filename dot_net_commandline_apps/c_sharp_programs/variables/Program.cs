d01127128339f52a6c620299614dae4bbae8f7dd
Aditya Kumar <aryan68125@gmail.com>
2023-12-07 19:03:34 +0530

    variables

--- dot_net_commandline_apps/c_sharp_programs/variables/Program.cs
+++ dot_net_commandline_apps/c_sharp_programs/variables/Program.cs
@@ -1,9 +1,21 @@
 ﻿namespace Aditya{
     class Variables{
         public static void Main(string[] args){
-            Console.WriteLine("There is a boy named Rollex");
-            Console.WriteLine("Rollex is a 20 year old boy");
-            Console.WriteLine("He really loves to program");
+            //using statically defined variables
+            string sentence1 = "There is a boy named Rollex";
+            int number = 20;
+            double double_variable = 50000.526;
+            Console.WriteLine(sentence1);
+            Console.WriteLine(number);
+            Console.WriteLine(double_variable);
+
+            //using dynamically defined variables
+            var sentence2 = "There is a boy named Aditya";
+            var number2 = 26;
+            var double_variable2 = 905621.526;
+            Console.WriteLine(sentence2);
+            Console.WriteLine(number2);
+            Console.WriteLine(double_variable2);
         }
     }
 }
