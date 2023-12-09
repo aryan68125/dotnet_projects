913824aaabe980272f59db56dcf4be43158125e4
Aditya Kumar <aryan68125@gmail.com>
2023-12-09 17:45:42 +0530

    how to take inputs from variables

---
+++ dot_net_commandline_apps/c_sharp_programs/get_user_input/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aditya{
+    class get_user_input{
+        public static void Main(string[] args){
+            //taking user input as a string
+            Console.WriteLine("Enter your name");
+            var x = Console.ReadLine();
+            Console.WriteLine($"Hello {x} what's up");
+
+            //How to convert a string into a number
+            var a = Convert.ToInt64(Console.ReadLine());
+            var b = Convert.ToInt64(Console.ReadLine());
+            var c = a+b;
+            Console.WriteLine($"sum  = {c}");
+        }
+    }
+}
