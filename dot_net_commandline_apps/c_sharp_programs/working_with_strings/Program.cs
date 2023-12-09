fd2311a64e04ede24642a6aebfab83a0fd8633dc
Aditya Kumar <aryan68125@gmail.com>
2023-12-09 16:43:43 +0530

    string methods

--- dot_net_commandline_apps/c_sharp_programs/working_with_strings/Program.cs
+++ dot_net_commandline_apps/c_sharp_programs/working_with_strings/Program.cs
@@ -8,6 +8,21 @@
             Console.WriteLine($"name : {name}, college_name : {college_name}, Address : {Addr} city: {city}");
             //number of character in a string
             Console.WriteLine($"name : {name.Length} college_name : {college_name.Length} Address : {Addr.Length} city: {city}");
+
+            //string methods
+            /*
+            ToUpper() //converts the string into uppercase
+            ToLower() //converts the string into lowercase
+            Contains("parameter_string") //we use this if a sentence or a word contains the word passed as a parameter
+            How to find the first character in a string
+            string_variable_name[0] ==> accessing first character in a string
+            */
+            Console.WriteLine(city.ToUpper());
+            Console.WriteLine(college_name.ToLower());
+            Console.WriteLine(Addr.Contains("colony"));
+            Console.WriteLine(name[0]);
+            Console.WriteLine(Addr.IndexOf("colony"));
+            Console.WriteLine(Addr.IndexOf("c"));
         }
     }
 }
