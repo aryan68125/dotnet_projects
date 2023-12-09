79d234c374f0180d4c239e67d9678b1d35b9c2d6
Aditya Kumar <aryan68125@gmail.com>
2023-12-09 16:24:21 +0530

    working with strings

---
+++ dot_net_commandline_apps/c_sharp_programs/working_with_strings/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Aditya{
+    class Strings_working{
+        public static void Main(string[] args){
+            string name = "Rollex";
+            var college_name = "SITM";
+            string Addr  = "2/446 sec-L LDA colony";
+            var city = "lucknow";
+            Console.WriteLine($"name : {name}, college_name : {college_name}, Address : {Addr} city: {city}");
+            //number of character in a string
+            Console.WriteLine($"name : {name.Length} college_name : {college_name.Length} Address : {Addr.Length} city: {city}");
+        }
+    }
+}
