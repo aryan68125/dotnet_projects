80c2e9fe92a4d898a98f5a09d8ce614a3787e568
Aditya Kumar <aryan68125@gmail.com>
2023-12-10 10:03:44 +0530

    basic array input and output program

---
+++ dot_net_commandline_apps/c_sharp_programs/Arrays/Array_basics/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Aditya
+{
+    class Array_Basics{
+        public static void Main(string[] args){
+            Console.WriteLine("Array Basics");
+            Console.WriteLine("Enter the size of the integer array");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int [] arr=new int[size];
+            Console.WriteLine("Enter elements in the array");
+            for(int i=0;i<arr.Length;i++){
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Display array Elements");
+            for(int i=0;i<arr.Length;i++){
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
