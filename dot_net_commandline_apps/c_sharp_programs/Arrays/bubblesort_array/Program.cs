dc0dd70a5f6cebfb6404c201e2d3644eb2ea9685
Aditya Kumar <aryan68125@gmail.com>
2023-12-10 15:18:50 +0530

    arrays/1. bubble sort 2. selection sort

---
+++ dot_net_commandline_apps/c_sharp_programs/Arrays/bubblesort_array/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Aditya{
+    class BubbleSort{
+        public static void Main(string[] args){
+            Console.WriteLine("BubbleSort Program Arrays");
+            for(int i=0;i<1000;i++){
+                Console.WriteLine("Enter the size of the array");
+                int size = Convert.ToInt32(Console.ReadLine());
+                int[] arr=new int[size];
+                //enter array elements
+                Console.WriteLine("Enter elements in the array");
+                EnterElements(arr, size);
+                //print the entered array elements by the user
+                Console.WriteLine("Display the elements of the array");
+                DisplayArray(arr, size);
+                //performing bubblesort
+                Console.WriteLine("Performing bubblesort ascending order");
+                BubbleSort_function(arr,size);
+                Console.WriteLine("Display array after bubblesort operation");
+                DisplayArray(arr,size);
+
+                Console.WriteLine("Do you want to continue y/n");                
+                string ch=Console.ReadLine();
+
+                if(ch=="n"){
+                    Console.WriteLine("exit bubblesort program");
+                    Console.WriteLine("Reached target location shutting down program");
+                    break;
+                }
+            }
+        }
+        public static void EnterElements(int[] arr, int size){
+            for (int i=0;i<size;i++){
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public static void DisplayArray(int[] arr, int size){
+            for(int i=0;i<size;i++){
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine("");
+        }
+        public static void BubbleSort_function(int[] arr, int size){
+            for(int i=0;i<size-1;i++){
+                for(int j=i+1;j<size;j++){
+                    if(arr[i]>arr[j]){
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ dot_net_commandline_apps/c_sharp_programs/Arrays/selectionsort/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Aditya{
+    class SelectionSort{
+        public static void Main(string[] args){
+            Console.WriteLine("SelectionSort program arrays");
+            for(int i=0;i<1000;i++){
+                Console.WriteLine("Enter the size of the array");
+                int size = Convert.ToInt32(Console.ReadLine());
+                int[] arr=new int[size];
+
+                //enter array elements
+                Console.WriteLine("Enter array elements");
+                EnterElements(arr,size);
+
+                //Display Array elements
+                Console.WriteLine("Display array elements");
+                DisplayArray(arr,size);
+
+                //Display array after performing selection_sort
+                Console.WriteLine("Performing selection sort on array");
+                SelectionSort_function(arr,size);
+                Console.WriteLine("Display array after selection sort is performed");
+                DisplayArray(arr,size);
+
+                Console.WriteLine("Do you want to continue y/n");
+                string ch = Console.ReadLine();
+                if(ch=="n"){
+                    Console.WriteLine("Reached target location exiting program now");
+                    break;
+                }
+            }
+        }
+        public static void EnterElements(int[] arr,int size){
+            for(int i=0;i<size;i++){
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public static void DisplayArray(int[] arr, int size){
+            for(int i=0;i<size;i++){
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine("");
+        }
+        public static void SelectionSort_function(int[] arr, int size){
+            for(int i=0;i<size-1;i++){
+                int minIndex=i;
+                for(int j=i+1;j<size;j++){
+                    if(arr[j]<arr[minIndex]){
+                        minIndex=j;
+                    }
+                }
+
+                //swap elements in array
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+}
