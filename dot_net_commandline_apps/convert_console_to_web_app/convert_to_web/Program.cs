5abc7d1075e9473ecb07fcb3479586c66bba6b44
Aditya Kumar <aryan68125@gmail.com>
2023-12-13 08:50:53 +0530

    how to convert console app to a web app

---
+++ dot_net_commandline_apps/convert_console_to_web_app/convert_to_web/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+namespace convert_to_web
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Hello world!");
+        }
+
+        //creating a host builder method
+        public static IHostBuilder CreateHostBuilder(string[] args){
+            Host.CreateHostBuilder(args);
+        } 
+    }
+}
